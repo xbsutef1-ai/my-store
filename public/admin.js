f1eeab5342c5392732c42c671085e2b98e672935
xbsutef1-ai <xbsutef1@gmail.com>
2025-12-21 11:55:11 +0300

    Update admin.js

--- public/admin.js
+++ public/admin.js
@@ -1,12 +1,3 @@
-fetch("/api/admin/products",{
-  headers:{Authorization:"Bearer "+localStorage.token}
-})
-.then(r=>r.json())
-.then(d=>{
-  products.innerHTML=d.map(p=>`
-    <div>
-      <b>${p.title}</b>
-      (${p.plans.length} plans)
-    </div>
-  `).join("");
-});
+fetch("/api/admin/products")
+  .then(r => r.json())
+  .then(d => list.innerHTML = JSON.stringify(d, null, 2));
