51f509689a67b2822688f2d43cbe62c04119730e
xbsutef1-ai <xbsutef1@gmail.com>
2025-12-24 01:10:11 +0300

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
-  email: { type: String, unique: true },
-  password: String,
-  name: String,
+  email: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+  name: { type: String, required: true },
   role: { type: String, default: "user" }
 }, { timestamps: true });
 
