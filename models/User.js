c4bf554dfaef1899c2a410a191931ed92804df20
xbsutef1-ai <xbsutef1@gmail.com>
2025-12-24 00:06:19 +0300

    Create User.js

---
+++ models/User.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+  role: { type: String, default: "user" } // user | admin | support
+}, { timestamps: true });
+
+export default mongoose.model("User", UserSchema);
